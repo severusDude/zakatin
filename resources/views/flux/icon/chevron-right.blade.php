12c31a224a2dac0691d8cb2d1e01f79983bddd0a
severusDude <fajarsaladin05@gmail.com>
2025-05-09 12:55:41 +0700

    feat(ui): add left and right arrow icon

---
+++ resources/views/flux/icon/chevron-right.blade.php
@@ -0,0 +1,21 @@
+@php $attributes = $unescapedForwardedAttributes ?? $attributes; @endphp
+
+@props([
+'variant' => 'outline',
+])
+
+@php
+$classes = Flux::classes('shrink-0')
+->add(match($variant) {
+'outline' => '[:where(&)]:size-6',
+'solid' => '[:where(&)]:size-6',
+'mini' => '[:where(&)]:size-5',
+'micro' => '[:where(&)]:size-4',
+});
+@endphp
+
+{{-- Your SVG code here: --}}
+<svg {{ $attributes->class($classes) }} data-flux-icon aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
